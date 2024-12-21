d79f41c42b2c40fc87c3cb4c51e65045cb43364f
ivanchiou <wyattkidd@gmail.com>
2024-12-21 16:08:16 +0800

    fix: add find by user id in demo model

--- src/main/java/com/example/demo/model/DemoRepository.java
+++ src/main/java/com/example/demo/model/DemoRepository.java
@@ -1,6 +1,11 @@
 package com.example.demo.model;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface DemoRepository extends JpaRepository<DemoModel, Long> {
+public interface DemoRepository extends JpaRepository<DemoModel, Integer> {
     DemoModel findByName(String name);
+
+    @Query("SELECT u FROM DemoModel u WHERE u.id = :id")
+    DemoModel findByID(@Param("id") int id);
 }
