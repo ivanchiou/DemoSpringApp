8e701c73f5cfab93bca106b6073597e0aea26a48
ivanchiou <wyattkidd@gmail.com>
2025-01-07 06:56:39 +0800

    feat: spring security authentication and authorization

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,6 +10,10 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import static org.springframework.security.config.Customizer.withDefaults;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -17,18 +23,39 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
-            .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authroize -> authroize
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() 
-                .requestMatchers("/api/**").authenticated()
+                .requestMatchers("/api/admin/users/**").hasAuthority("ROLE_ADMIN")
+                .requestMatchers("/api/users/**").hasAuthority("ROLE_USER")                
+                .requestMatchers("/page/**").authenticated()
                 .anyRequest().permitAll())
-            .exceptionHandling(ex -> ex
-                .authenticationEntryPoint((request, response, authException)->{
-                    System.err.println("401 error");
-                    response.sendRedirect("/login"); // 導向預設登入頁
-                }));
+            .formLogin(form -> form
+                .loginPage("/page/login")
+                .loginProcessingUrl("/api/auth/login")
+                .defaultSuccessUrl("/page/example", true)
+                .permitAll()
+            );
         
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        // 配置 JdbcUserDetailsManager 並設置查詢語句（如果需要自定義）
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // 默認查詢語句（可省略，只有當資料表結構不一致時才需修改）
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+            "SELECT username, password, enabled FROM users WHERE username = ?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+            "SELECT username, authority FROM authorities WHERE username = ?");
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }   
--- src/main/java/com/example/demo/controller/APIController.java
+++ src/main/java/com/example/demo/controller/APIController.java
@@ -4,14 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
 import com.example.demo.model.DemoModel;
 import com.example.demo.service.DemoService;
+import java.util.Map;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @RestController
 @Tag(name = "API", description = "Operations for APIController")
@@ -21,10 +31,32 @@ public class APIController {
     @Autowired
     private DemoService demoService;
 
+    @Autowired
+    private AuthenticationManager authenticaitonManager;
+
     @Operation(summary = "Test API hello", description = "Test API hello")
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, Swagger!";
+    }
+
+    @Operation(summary = "Login API", description = "Login API")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "User login successfully"),
+        @ApiResponse(responseCode = "400", description = "username or password is null or empty") // bad request
+    })
+    @PostMapping("/auth/login")
+    public ResponseEntity<?> login(@RequestParam String username, @RequestParam String password) {
+        if(username == null || username.trim().isEmpty() || password == null || password.trim().isEmpty()) {
+            return ResponseEntity.badRequest().body(Map.of("error", "username or password can't be null or empty"));
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+
+        Authentication authentication = authenticaitonManager.authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return ResponseEntity.ok(Map.of("success", "Login successful"));
     }
 
     @Operation(summary = "Get user model by ID", description = "Get user model by ID")
@@ -33,11 +65,19 @@ public class APIController {
             @ApiResponse(responseCode = "400", description = "Invalid input provided") // bad request
     })
     @GetMapping("/users/{id}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public DemoModel getUserByID(@PathVariable int id) {
         DemoModel demoModel = this.demoService.getUserByID(id);
         if(demoModel == null) {
             throw new RuntimeException("demoModel can't be null");
         }
         return demoModel;
-    }   
+    }
+
+    @GetMapping("/admin/users/{id}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public Map<String, Boolean> setUserNameByID(@PathVariable int id, @RequestParam String name) {
+        boolean isSuccess = this.demoService.updateUserName(id, name);
+        return Map.of("success", isSuccess);
+    }
 }
