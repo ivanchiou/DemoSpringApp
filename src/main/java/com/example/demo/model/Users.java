f3f93386e35901cd012ada7c4d065d95f22d201e
ivanchiou <wyattkidd@gmail.com>
2025-01-20 17:34:34 +1100

    feat: add index to column email in users table

--- src/main/java/com/example/demo/model/Users.java
+++ src/main/java/com/example/demo/model/Users.java
@@ -4,15 +4,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Index;
 
 /**
  * Represents a User entity mapped to the database.
  */
 @Entity(name = "users")
+@Table(name = "users", indexes = {
+    @Index(name = "idx_email", columnList = "email")
+})
 public class Users {
     @Id
     private String username;
-    
+
+    private String email;
     private String password;
     private boolean enabled;
 
@@ -23,6 +29,14 @@ public class Users {
 
     public void setName(String username) {
         this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
