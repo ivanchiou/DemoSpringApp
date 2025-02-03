fef76714237327dfe36b5ad66d5d952813bb5530
ivanchiou <wyattkidd@gmail.com>
2025-02-03 12:27:26 +1100

    feat: mongodb connection pool settings

--- src/main/java/com/example/demo/config/MongoDBConfig.java
+++ src/main/java/com/example/demo/config/MongoDBConfig.java
@@ -3,7 +3,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Value;
+
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
@@ -19,7 +24,18 @@ public class MongoDBConfig {
     
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create(mongoUri);
+        return MongoClients.create(
+            MongoClientSettings.builder()
+                .applyConnectionString(new com.mongodb.ConnectionString(mongoUri))
+                .applyToConnectionPoolSettings(builder ->
+                    builder
+                        .minSize(5)  // 最小連接數
+                        .maxSize(50) // 最大連接數
+                        .maxWaitTime(5000, TimeUnit.MILLISECONDS) // 最大等待時間
+                        .maxConnectionIdleTime(30000, TimeUnit.MILLISECONDS) // 最大閒置時間
+                        .maxConnectionLifeTime(60000, TimeUnit.MILLISECONDS) // 最大存活時間
+                )
+                .build());
     }
 
     @Bean
